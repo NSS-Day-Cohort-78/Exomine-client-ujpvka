d7850dfc100dd48b597e16c18c64627ae788d7f0
Deniz Koc <dkocusa@outlook.com>
2025-08-19 10:47:42 -0500

    Governors: use local API; Facilities: initial dropdown; main wiring

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,16 @@
+import { Facilities } from "./Facilities.js"
 import { Governors } from "./Governors.js"
 
 const render = async () => {
     const governorsHTML = await Governors()
+    const miningFacilitiesHTML = await Facilities()
 
     const container = document.getElementById("container")
 
     container.innerHTML = `
     <h1>Solar System Mining Marketplace</h1>
     ${governorsHTML}
+    ${miningFacilitiesHTML}
     `
 }
 

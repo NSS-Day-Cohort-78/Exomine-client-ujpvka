a5bb8603fd4a5d8f6e357b67aacf86632898818a
James Heaton <heaton1138@gmail.com>
2025-08-19 14:36:30 -0500

    added dummy data / oops

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,19 @@
+import { Facilities } from "./Facilities.js"
 import { Governors } from "./Governors.js"
+import { ColoniesInventory } from "./ColoniesInventory.js"
 
 const render = async () => {
     const governorsHTML = await Governors()
+    const miningFacilitiesHTML = await Facilities()
+    const colonyInventoryHTML = await ColoniesInventory()
 
     const container = document.getElementById("container")
 
     container.innerHTML = `
     <h1>Solar System Mining Marketplace</h1>
     ${governorsHTML}
+    ${miningFacilitiesHTML}
+    ${colonyInventoryHTML}
     `
 }
 

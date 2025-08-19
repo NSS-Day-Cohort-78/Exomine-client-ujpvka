9fb737c6ba53ff03977444b1b55bf1b11d5c7c2c
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-08-19 09:19:09 -0500

    added class label and correct url

--- scripts/Governors.js
+++ scripts/Governors.js
@@ -1,17 +1,17 @@
 export const Governors = async () => {
     // dummy api
-    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const response = await fetch("http://localhost:8088/governors")
     const governors = await response.json()
 
     let html = `
-    <section>
-        <p>Choose a governor</p>
-        <select>
+    <section class='dropdown-container'>
+        <label for='choose-governor'>Choose a governor</label>
+        <select id='choose-governor'>
     `
     html += governors
         .map((governor) => {
             return `
-        <option>${governor.name}</option>
+        <option value='${governor.id}'>${governor.name}</option>
         `
         })
         .join("")

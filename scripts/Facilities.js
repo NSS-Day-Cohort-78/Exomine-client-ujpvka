2b9c66f0a75f7baf516be4823308598a82d5d8d3
Deniz Koc <dkocusa@outlook.com>
2025-08-20 13:26:09 -0500

    facilities drop down

--- scripts/Facilities.js
+++ scripts/Facilities.js
@@ -1,21 +1,33 @@
-import { setFacilityId } from "./TransientState.js"
-
+// scripts/Facilities.js
+import { setFacilityId, getFacilityTransientState, getColonyTransientState } from "./TransientState.js"
 
 export const Facilities = async () => {
+  // API'den facilities çek
   const response = await fetch("http://localhost:8088/miningFacilities")
   const facilities = await response.json()
 
+ 
+  const transientFacility = getFacilityTransientState()
+  const transientColony   = getColonyTransientState()
+
+  
+  const disabledAttr = transientColony.colonyId === 0 ? "disabled" : ""
+
   let html = `
-  <section class="dropdown-container">
-    <label for="choose-facility">Choose a facility</label>
-    <select id="choose-facility">
-      <option value="0">Choose</option>
+  <section class='dropdown-container'>
+    <label for='choose-facility'>Choose a facility</label>
+    <select id='choose-facility' ${disabledAttr}>
+      <option value='0' ${transientFacility.miningFacilityId === 0 ? "selected" : ""} ${disabledAttr}>Choose</option>
   `
 
-  html += facilities
-    .map(facility => `
-      <option value="${facility.id}">${facility.name}</option>
-    `)
+  
+  html += facilities.map(facility => {
+      const selected = transientFacility.miningFacilityId === facility.id ? "selected" : ""
+      const optDisabled = facility.active ? "" : "disabled"
+      return `
+        <option value='${facility.id}' ${selected} ${optDisabled}>${facility.name}</option>
+      `
+    })
     .join("")
 
   html += `
@@ -26,17 +38,11 @@ export const Facilities = async () => {
   return html
 }
 
-
-document.addEventListener("change", eventHandler)
-
-async function eventHandler(e) {
+// governors'taki gibi basit change handler
+document.addEventListener("change", (e) => {
   if (e.target.id === "choose-facility") {
-    const chosenId = parseInt(e.target.value, 10)
-
+    const id = parseInt(e.target.value, 10)
+    setFacilityId(id)                         
     
-    setFacilityId(chosenId)
-
-    
-    document.dispatchEvent(new CustomEvent("stateChanged"))
   }
-}
+})
--- scripts/FacilityInventory.js
+++ scripts/FacilityInventory.js
@@ -8,7 +8,7 @@
 // - once minerals are selected the Purchase mineral button is enabled AND 1 ton is deducted from facilityMinerals and 1 ton is added to the colony's inventory
 
 
-import { setColonyMineralId, setFacilityMineralId, getFacilityTransientState, setFacilityTons } from './TransientState.js'
+/*import { setColonyMineralId, setFacilityMineralId, getFacilityTransientState, setFacilityTons } from './TransientState.js'
 
 
 
@@ -68,4 +68,56 @@ export const FacilityInventory = async () => {
     return html
 
 
+}*/
+// FacilityInventory.js
+import { getFacilityTransientState, setFacilityMineralId, setColonyMineralId } from "./TransientState.js"
+
+export const FacilityInventory = async () => {
+  const response = await fetch(
+    "http://localhost:8088/facilityInventory?_expand=mineral&_expand=miningFacility"
+  )
+  const inventoryItems = await response.json()
+
+  const facilityState = getFacilityTransientState()
+  const facilityId = facilityState.miningFacilityId
+
+  let html = `
+    <div class="Facility-Inventory-input">
+  `
+
+  // Header
+  if (facilityId > 0) {
+    const matched = inventoryItems.filter(i => i.miningFacilityId === facilityId)
+    const facilityName = matched[0]?.miningFacility?.name || ""
+    html += `<h2>Facility Minerals for ${facilityName}</h2>`
+
+    // Radio list (tons > 0)
+    html += matched
+      .filter(i => i.tons > 0)
+      .map(i => `
+        <div>
+          <input type="radio" name="choose-inventory" value="${i.mineralId}" />
+          ${i.tons} tons of ${i.mineral.name}
+        </div>
+      `)
+      .join("")
+  } else {
+    html += `<h2>Facility Minerals</h2>`
+  }
+
+  html += `
+    </div>
+  `
+  return html
 }
+
+// change listener
+document.addEventListener("change", (e) => {
+  if (e.target.name === "choose-inventory") {
+    const mineralId = parseInt(e.target.value, 10)
+    setFacilityMineralId(mineralId)
+    setColonyMineralId(mineralId)
+    document.dispatchEvent(new CustomEvent("stateChanged"))
+  }
+})
+
